fed9f0a47ddc6aa052eb2d2689b44ce0ed5fc25e
mooreBrendan <moore.brendanm@comcast.net>
2022-12-16 00:10:49 -0500

    made bot class to better organize code

--- bot.py
+++ bot.py
@@ -1,3 +1,96 @@
+from string import ascii_lowercase
+
+
 class Bot:
-  def __init__(self, file):
-    self.words = file
+  def __init__(self, file, print_d=False):
+    self.words = self.get_words(file)
+    self._print = print_d
+
+  def get_words(self, fname):
+    file = open(fname)
+    words = file.read().split("\n")
+    return words
+
+  def check_move(self, letter, index, word):
+    cleaned = word[:index]+word[index+1:]
+    return letter in cleaned
+
+  def check_doubles(self, new_guess, pattern):
+    temp = []
+    for i, e in enumerate(new_guess):
+      if pattern[i] == "x":
+        continue
+      elif pattern[i] == "o":
+        continue
+      temp.append(e)
+    return temp
+
+  def check(self, prev_guess, pattern, new_guess):
+    for index, pat in enumerate(pattern):
+      if pat == "_":
+        temp = self.check_doubles(new_guess, pattern)
+        if prev_guess[index] in temp:
+          return False
+      elif pat == "o":
+        # check for difference
+        if new_guess[index] == prev_guess[index]:
+          return False
+        if not self.check_move(prev_guess[index], index, new_guess):
+          return False
+      elif pat == "x":
+        if new_guess[index] != prev_guess[index]:
+          return False
+
+    return True
+
+  def cull_list(self, prev_guess, pattern):
+    return [i for i in self.words if self.check(prev_guess, pattern, i)]
+
+  def prompt(self):
+    x = input("enter word:")
+    y = input(
+        "enter pattern ('_' = not in word, 'o' = wrong spot, 'x'=correct:")
+    return x, y
+
+  def check_input(self, guess, pattern):
+    if (len(guess) != 5):
+      return False
+    if (len(pattern) != 5):
+      return False
+    for i in guess.lower():
+      if i not in ascii_lowercase:
+        return False
+    for i in pattern.lower():
+      if i not in ["_", "o", "x"]:
+        return False
+    return True
+
+  def get_input(self):
+    cond = False
+    while not cond:
+      guess, pattern = self.prompt()
+      cond = self.check_input(guess, pattern)
+    return guess, pattern
+
+  def get_word(self):
+    if len(self.words) < 1:
+      raise ValueError("WORD LIST EMPTY!!!!")
+    return self.words[0]
+
+  def run(self):
+    for _ in range(6):
+      word_choie = self.get_word()
+      if self._print:
+        print(f"word space:{len(self.words)}")
+        print(word_choie)
+      if len(self.words) == 1 and self._print:
+        print("FINAL ANSWER")
+        return
+      guess, pattern = self.get_input()
+      if pattern == "xxxxx" and self._print:
+        print("congratulations!")
+        return
+      self.words = self.cull_list(guess, pattern)
+
+    if self._print:
+      print("failed game")
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from main import check, check_move
+from bot import Bot
 
 
 class bcolors:
@@ -13,99 +13,101 @@ class bcolors:
   UNDERLINE = '\033[4m'
 
 
-def test_check(guess, pattern, word, expected):
-  if check(guess, pattern, word) != expected:
+def test_check(bot, guess, pattern, word, expected):
+  if bot.check(guess, pattern, word) != expected:
     print(bcolors.FAIL +
           f"FAILED:{guess=},{pattern=},{word=},{expected=}"+bcolors.ENDC)
     return False
   return True
 
 
-def test_check_move(letter, index, word, expected):
-  if check_move(letter, index, word) != expected:
+def test_check_move(bot, letter, index, word, expected):
+  if bot.check_move(letter, index, word) != expected:
     print(bcolors.FAIL +
           f"FAILED:{letter=},{index=},{word=},{expected=}"+bcolors.ENDC)
     return False
   return True
 
 
-def test_check_move_all():
+def test_check_move_all(bot):
   print("\n"+bcolors.OKCYAN+bcolors.BOLD+"Testing check_move"+bcolors.ENDC)
   tests = [
-      test_check_move("l", 1, "close", False),
-      test_check_move("t", 4, "stent", True),  # second t ok, checked elsewhere
-      test_check_move("c", 0, "close", False)
+      # second t ok, checked elsewhere
+      test_check_move(bot, "l", 1, "close", False),
+      test_check_move(bot, "t", 4, "stent", True),
+      test_check_move(bot, "c", 0, "close", False)
   ]
   print(bcolors.OKGREEN+f"correct:{sum(tests)}"+bcolors.ENDC)
   print(bcolors.FAIL+f"failed:{len(tests)-sum(tests)}"+bcolors.ENDC)
 
 
-def test_check_all():
+def test_check_all(bot):
   print("\n"+bcolors.OKCYAN+bcolors.BOLD+"Testing check_move"+bcolors.ENDC)
   tests = [
       # check placement for empty
-      test_check("irate", "_____", "ioooo", False),
-      test_check("irate", "_____", "orooo", False),
-      test_check("irate", "_____", "ooaoo", False),
-      test_check("irate", "_____", "oooto", False),
-      test_check("irate", "_____", "ooooe", False),
-      test_check("irate", "_____", "ooooo", True),
+      test_check(bot, "irate", "_____", "ioooo", False),
+      test_check(bot, "irate", "_____", "orooo", False),
+      test_check(bot, "irate", "_____", "ooaoo", False),
+      test_check(bot, "irate", "_____", "oooto", False),
+      test_check(bot, "irate", "_____", "ooooe", False),
+      test_check(bot, "irate", "_____", "ooooo", True),
 
       # check fail for move:
-      test_check("irate", "ooooo", "ioooo", False),
-      test_check("irate", "ooooo", "orooo", False),
-      test_check("irate", "ooooo", "ooaoo", False),
-      test_check("irate", "ooooo", "oooto", False),
-      test_check("irate", "ooooo", "ooooe", False),
-      test_check("irate", "ooooo", "ooooo", False),
+      test_check(bot, "irate", "ooooo", "ioooo", False),
+      test_check(bot, "irate", "ooooo", "orooo", False),
+      test_check(bot, "irate", "ooooo", "ooaoo", False),
+      test_check(bot, "irate", "ooooo", "oooto", False),
+      test_check(bot, "irate", "ooooo", "ooooe", False),
+      test_check(bot, "irate", "ooooo", "ooooo", False),
 
       # check placement for move
-      test_check("irate", "o____", "ioooo", False),
-      test_check("irate", "_o___", "orooo", False),
-      test_check("irate", "__o__", "ooaoo", False),
-      test_check("irate", "___o_", "oooto", False),
-      test_check("irate", "____o", "ooooe", False),
-      test_check("irate", "o____", "iiooo", False),
-      test_check("irate", "_o___", "orroo", False),
-      test_check("irate", "__o__", "ooaao", False),
-      test_check("irate", "___o_", "ooott", False),
-      test_check("irate", "____o", "oooee", False),
-      test_check("irate", "o____", "oiooo", True),
-      test_check("irate", "_o___", "ooroo", True),
-      test_check("irate", "__o__", "oooao", True),
-      test_check("irate", "___o_", "oooot", True),
-      test_check("irate", "____o", "oooeo", True),
+      test_check(bot, "irate", "o____", "ioooo", False),
+      test_check(bot, "irate", "_o___", "orooo", False),
+      test_check(bot, "irate", "__o__", "ooaoo", False),
+      test_check(bot, "irate", "___o_", "oooto", False),
+      test_check(bot, "irate", "____o", "ooooe", False),
+      test_check(bot, "irate", "o____", "iiooo", False),
+      test_check(bot, "irate", "_o___", "orroo", False),
+      test_check(bot, "irate", "__o__", "ooaao", False),
+      test_check(bot, "irate", "___o_", "ooott", False),
+      test_check(bot, "irate", "____o", "oooee", False),
+      test_check(bot, "irate", "o____", "oiooo", True),
+      test_check(bot, "irate", "_o___", "ooroo", True),
+      test_check(bot, "irate", "__o__", "oooao", True),
+      test_check(bot, "irate", "___o_", "oooot", True),
+      test_check(bot, "irate", "____o", "oooeo", True),
 
       # check exact
-      test_check("irate", "xxxxx", "ioooo", False),
-      test_check("irate", "xxxxx", "orooo", False),
-      test_check("irate", "xxxxx", "ooaoo", False),
-      test_check("irate", "xxxxx", "oooto", False),
-      test_check("irate", "xxxxx", "ooooe", False),
-      test_check("irate", "xxxxx", "ooooo", False),
+      test_check(bot, "irate", "xxxxx", "ioooo", False),
+      test_check(bot, "irate", "xxxxx", "orooo", False),
+      test_check(bot, "irate", "xxxxx", "ooaoo", False),
+      test_check(bot, "irate", "xxxxx", "oooto", False),
+      test_check(bot, "irate", "xxxxx", "ooooe", False),
+      test_check(bot, "irate", "xxxxx", "ooooo", False),
 
       # check placement for exact:
-      test_check("irate", "x____", "ioooo",	True),
-      test_check("irate", "_x___", "orooo",	True),
-      test_check("irate", "__x__", "ooaoo",	True),
-      test_check("irate", "___x_", "oooto",	True),
-      test_check("irate", "____x", "ooooe",	True),
+      test_check(bot, "irate", "x____", "ioooo",	True),
+      test_check(bot, "irate", "_x___", "orooo",	True),
+      test_check(bot, "irate", "__x__", "ooaoo",	True),
+      test_check(bot, "irate", "___x_", "oooto",	True),
+      test_check(bot, "irate", "____x", "ooooe",	True),
 
       # check doubles (zero correct)
 
       # check doubles (one correct)
 
       # random tests:
-      test_check("irate", "__xxx", "crate", False),
-      test_check("irate", "__xxx", "elate", True),
-      test_check("irate", "xx_x_", "irote", False),
-      test_check("irate", "xx_x_", "irats", False),
-      test_check("irate", "xx_x_", "irots", True)
+      test_check(bot, "irate", "__xxx", "crate", False),
+      test_check(bot, "irate", "__xxx", "elate", True),
+      test_check(bot, "irate", "xx_x_", "irote", False),
+      test_check(bot, "irate", "xx_x_", "irats", False),
+      test_check(bot, "irate", "xx_x_", "irots", True)
   ]
   print(bcolors.OKGREEN+f"correct:{sum(tests)}"+bcolors.ENDC)
   print(bcolors.FAIL+f"failed:{len(tests)-sum(tests)}"+bcolors.ENDC)
 
 
 if __name__ == "__main__":
-  test_check_move_all()
-  test_check_all()
+  test_bot = Bot("valid-wordle-words.txt")
+  test_check_move_all(test_bot)
+  test_check_all(test_bot)
